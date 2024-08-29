6fa27696141d3d3aedd99edf18054459a7dbd5c6
Michael Rudenko <annoyedmichael@gmail.com>
2024-08-29 11:59:54 +0300

    switch added

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,9 +1,17 @@
-import { Text, View } from "react-native"
+import { Switch, Text, View } from "react-native"
+import { useColorScheme } from "nativewind"
 
 export default function SettingsScreen() {
+  const { colorScheme, toggleColorScheme } = useColorScheme()
+  console.log(colorScheme)
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Settings Screen</Text>
+      <Text className="text-3xl">Toggle theme</Text>
+      <Switch
+        value={colorScheme === "dark"}
+        onValueChange={toggleColorScheme}
+      />
     </View>
   )
 }
