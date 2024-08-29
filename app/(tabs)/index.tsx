8293f682b9968d7665bc4b3d8e127ecf39b13421
Michael Rudenko <annoyedmichael@gmail.com>
2024-08-29 11:51:22 +0300

    intial comit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,21 @@
+import { Text, View, Button } from "react-native"
+import { useState } from "react"
+export default function HomeScreen() {
+  const [count, setCount] = useState(0)
+  function incrementCounter() {
+    setCount((prev) => prev + 1)
+  }
+
+  return (
+    <View className="flex flex-col justify-around items-center h-full">
+      <Text className="text-3xl font-bold">Mega clicker</Text>
+      <Text className="text-3xl font-bold">{count}</Text>
+      <View className="w-[50%] h-20 bg-red-300 border-2 rounded-xl flex justify-center items-center">
+        <Button
+          title="Click"
+          onPress={incrementCounter}
+        />
+      </View>
+    </View>
+  )
+}
